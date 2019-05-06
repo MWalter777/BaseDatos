9e2cef203583ae2a5745beb766c8e6fdbbc76fea
NardoXD <lopezcesar292@gmail.com>
2019-05-06 01:42:20 -0600

    CRUD Roles con asignacion de permisos

---
+++ SAP/SAP/Controllers/ROLsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SAP.Models;
+using EntityState = System.Data.Entity.EntityState;
+
+namespace SAP.Controllers
+{
+    public class ROLsController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: ROLs
+        public ActionResult Index()
+        {
+            return View(db.ROL.ToList());
+        }
+
+        // GET: ROLs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ROL rOL = db.ROL.Find(id);
+            if (rOL == null)
+            {
+                return HttpNotFound();
+            }
+            IEnumerable<PERMISO> permisos = db.PERMISO.ToList();
+            IEnumerable<PERMITE> permisos_activos = db.PERMITE.Where(p => p.ID_ROL == id).ToList();
+            List<int> id_permisos = new List<int>();
+
+            foreach (var permiso in permisos_activos)
+            {
+                id_permisos.Add(permiso.ID_PERMISO);
+            }
+
+            ViewBag.permisos = permisos;
+            ViewBag.permisos_activos = id_permisos;
+
+            return View(rOL);
+        }
+
+        // GET: ROLs/Create
+        public ActionResult Create()
+        {
+            IEnumerable<PERMISO> permisos = db.PERMISO.ToList();
+            ViewBag.permisos = permisos;
+
+            return View();
+        }
+
+        // POST: ROLs/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_ROL,NOMBRE_ROL,DESCRIPCION_ROL")] ROL rOL, int[] id_permiso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ROL.Add(rOL);
+                db.SaveChanges();
+
+                foreach (var id in id_permiso)
+                {
+                    db.PERMITE.Add(new PERMITE { ID_ROL = rOL.ID_ROL, ID_PERMISO = id });
+                }
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rOL);
+        }
+
+        // GET: ROLs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ROL rOL = db.ROL.Find(id);
+            if (rOL == null)
+            {
+                return HttpNotFound();
+            }
+
+            IEnumerable<PERMISO> permisos = db.PERMISO.ToList();
+            IEnumerable<PERMITE> permisos_activos = db.PERMITE.Where(p => p.ID_ROL == id).ToList();
+            List<int> id_permisos = new List<int>();
+
+            foreach (var permiso in permisos_activos)
+            {
+                id_permisos.Add(permiso.ID_PERMISO);
+            }
+
+            ViewBag.permisos = permisos;
+            ViewBag.permisos_activos = id_permisos;
+
+            return View(rOL);
+        }
+
+        // POST: ROLs/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_ROL,NOMBRE_ROL,DESCRIPCION_ROL")] ROL rOL, int[] id_permiso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rOL).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(rOL);
+        }
+
+        // GET: ROLs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ROL rOL = db.ROL.Find(id);
+            if (rOL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rOL);
+        }
+
+        // POST: ROLs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            IEnumerable<PERMITE> borrar = db.PERMITE.Where(p => p.ID_ROL == id).ToList();
+            db.PERMITE.RemoveRange(borrar);
+            db.SaveChanges();
+
+            ROL rOL = db.ROL.Find(id);
+            db.ROL.Remove(rOL);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
