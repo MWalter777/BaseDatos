418daa64817f8ad88f853c2b0628381427500c0a
Luis Quijada <jose.jlq@hotmail.com>
2019-05-06 01:21:11 -0600

    crud PROFESION 100

---
+++ SAP/SAP/Controllers/PROFESIONController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SAP.Models;
+using SAP.Security;
+using System.Data.Entity.Infrastructure;
+
+namespace SAP.Controllers
+{
+    public class PROFESIONController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: PROFESION
+        public ActionResult Index()
+        {
+            return View(db.PROFESION.ToList());
+        }
+
+        // GET: PROFESION/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PROFESION profesion = db.PROFESION.Find(id);
+            if (profesion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profesion);
+        }
+
+        // GET: PROFESION/Create
+        public ActionResult Create()
+        {
+            PopulatePROFESIONDropDownList();
+            return View();
+        }
+
+        // POST: PROFESION/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_PROFESION,NOMBRE_PROFESION")] PROFESION profesion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PROFESION.Add(profesion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            PopulatePROFESIONDropDownList(profesion.ID_PROFESION);
+            return View(profesion);
+        }
+
+        // GET: PROFESION/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PROFESION profesion = db.PROFESION.Find(id);
+            if (profesion == null)
+            {
+                return HttpNotFound();
+            }
+            PopulatePROFESIONDropDownList(profesion.ID_PROFESION);
+            return View(profesion);
+        }
+
+        // POST: PROFESION/Edit/5
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPost(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var profesionToUpdate = db.PROFESION.Find(id);
+            if (TryUpdateModel(profesionToUpdate, "",
+               new string[] { "NOMBRE_PROFESION" }))
+            {
+                try
+                {
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (RetryLimitExceededException /* dex */)
+                {
+                    //Log the error (uncomment dex variable name and add a line here to write a log.
+                    ModelState.AddModelError("", "No es posible guardar los cambios.");
+                }
+            }
+            PopulatePROFESIONDropDownList(profesionToUpdate.ID_PROFESION);
+            return View(profesionToUpdate);
+        }
+
+        private void PopulatePROFESIONDropDownList(object selectedPROFESION = null)
+        {
+            var PROFESIONQuery = from d in db.PROFESION
+                                 orderby d.NOMBRE_PROFESION
+                                 select d;
+            ViewBag.ID_PROFESION = new SelectList(PROFESIONQuery, "ID_PROFESION", "NOMBRE_PROFESION", selectedPROFESION);
+        }
+
+        // GET: PROFESION/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PROFESION profesion = db.PROFESION.Find(id);
+            if (profesion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profesion);
+        }
+
+        // POST: PROFESION/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                PROFESION profesion = db.PROFESION.Find(id);
+                db.PROFESION.Remove(profesion);
+                db.SaveChanges();
+            }
+
+            catch (Exception)
+            {
+                ViewBag.error = "No se puede eliminar, hacer referencia a otra clase";
+
+                return View("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
