45056f7c8ce72dc2a3c6b38f21e84da7097d48c3
NardoXD <lopezcesar292@gmail.com>
2019-06-10 13:51:10 -0600

    Media validacion de campos

--- SAP/SAP/Scripts/js-own/script.js
+++ SAP/SAP/Scripts/js-own/script.js
@@ -1,8 +1,22 @@
 ﻿$(function (){
-	var campos = $("#create-form");
-	console.log(campos);
-	var deley = campos.children("#create_deley");
-	deley.click(function () {
 
+	$("#DELEY_DESCUENTO").change(function () {
+		if (this.checked) {
+			$("#create_porcentaje").show();
+			$("#PORCENTAJE").val("").prop('required', true);
+			$("#create_descuento").hide().prop('required', false);
+			$("#create_fechainicio").hide().prop('required', false);
+			$("#create_fechafin").hide().prop('required', false);
+		} else {
+			$("#create_porcentaje").hide();
+			$("#PORCENTAJE").val("").prop('required', false);
+			$("#DESCUENTO").val("").prop('required', true);
+			$("#FECHA_INICIO").val("").prop('required', true);
+			$("#FECHA_FIN").val("").prop('required', true);
+			$("#create_descuento").show();
+			$("#create_fechainicio").show();
+			$("#create_fechafin").show();
+		}
 	});
+	
 });
